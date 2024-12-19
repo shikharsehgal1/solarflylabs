699d83cbd3dd9579386620bf671ebf3bba6044fb
shikhar sehgal <106230191+shikharsehgal1@users.noreply.github.com>
2024-12-19 15:38:53 +0530

    Update and rename src/components/App.jsx to src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import EnergyUsage from 'EnergyUsage';
-import EnergyPrice from 'EnergyPrice';
+import EnergyUsage from './components/EnergyUsage';
+import EnergyPrice from './components/EnergyPrice';
 
 function App() {
   return (
